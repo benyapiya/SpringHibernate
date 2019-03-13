45bdcb181867f95dfca02bae2209eb9dcdb6336c
i852081 <benya.piyathamsoontorn@sap.com>
2019-03-13 10:49:32 -0400

    implement webservice

---
+++ src/main/java/com/journaldev/hibernate/main/EmployerForm.java
@@ -0,0 +1,22 @@
+package com.journaldev.hibernate.main;
+
+
+public class EmployeeForm {
+    private String username;
+    private String hobby;
+
+    public EmployeeForm() {
+    }
+  	public String getUsername() {
+  		return username;
+  	}
+  	public void setUsername(String username) {
+  		this.username = username;
+  	}
+  	public String getHobby() {
+  		return hobby;
+  	}
+  	public void setHobby(String hobby) {
+  		this.hobby = hobby;
+  	}
+}
---
+++ src/main/java/com/journaldev/hibernate/main/HibernateWeb.java
@@ -0,0 +1,49 @@
+package com.journaldev.hibernate.main;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+
+import com.journaldev.hibernate.model.Employee;
+import com.journaldev.hibernate.util.HibernateUtil;
+import com.journaldev.hibernate.main.EmployeeForm;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class HibernateWeb {
+
+		private String username;
+		private String hobby;
+    @PostMapping("/add_entry")
+    public String add_entry(@RequestBody EmployeeForm employeeForm) {
+
+			username = employeeForm.getUsername();
+			hobby = employeeForm.getHobby();
+  		Employee emp = new Employee();
+			emp.setName(username);
+			emp.setRole(hobby);
+			emp.setInsertTime(new Date());
+
+			//Get Session
+			Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+			//start transaction
+			session.beginTransaction();
+			//Save the Model object
+			session.save(emp);
+			//Commit transaction
+			session.getTransaction().commit();
+			System.out.println("Employee ID="+emp.getId());
+
+			//terminate session factory, otherwise program won't end
+			HibernateUtil.getSessionFactory().close();
+			return "SUCCESS";
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateWeb.class, args);
+	}
+
+}
