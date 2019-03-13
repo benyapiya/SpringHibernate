0a05772637dde5580234c37bf449eff39d32e8cc
benyapiya <benya.piya@gmail.com>
2019-03-13 15:49:32 -0400

    add cross origin in response header

--- src/main/java/com/journaldev/hibernate/main/HibernateWeb.java
+++ src/main/java/com/journaldev/hibernate/main/HibernateWeb.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import org.hibernate.Query;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @EnableAutoConfiguration
 public class HibernateWeb {
