f15bcdc98274e574aa0a80c1f99539754d660f5d
i852081 <benya.piyathamsoontorn@sap.com>
2019-03-13 20:22:34 -0400

    change delimeter

--- src/main/java/com/journaldev/hibernate/main/HibernateWeb.java
+++ src/main/java/com/journaldev/hibernate/main/HibernateWeb.java
@@ -46,7 +46,7 @@ public class HibernateWeb {
 			String db_records="";
 			for (int i = 0; i < list.size(); i++) {
 				Employee e = (Employee) list.get(i);
-			  db_records=db_records+"<br></br>"+e.getName()+", "+e.getRole();
+			  db_records=db_records+"::"+e.getName()+", "+e.getRole();
 			}
 
 			//terminate session factory, otherwise program won't end
